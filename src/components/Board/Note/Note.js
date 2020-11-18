e184e4f7f2d25e3dbcea16ea611c8866ab0bab23
giuliaeremite <prsgiulia@gmail.com>
2020-11-18 12:12:53 +0100

    removed unnecessary div container

--- src/components/Board/Note/Note.js
+++ src/components/Board/Note/Note.js
@@ -8,9 +8,7 @@ function Note({ onDelete, id, timestamp, content }) {
   return (
     <div className="Note">
       <span className="Note-timestamp">{timestamp}</span>
-      <div>
       <div className="Note-content">{parse(content)}</div>
-      </div>
       <button className="Note-delete-btn" onClick={() => onDelete(id)}>
         <FaTrash className="Note-delete-icon" />
       </button>
